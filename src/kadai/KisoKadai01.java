8da827aa57d54a093a85c28af5373403db8c29c9
Masaki <vkiie8564hbgfg@gmail.com>
2016-01-26 12:00:04 +0900

    課題０１修正

--- src/kadai/KisoKadai01.java
+++ src/kadai/KisoKadai01.java
@@ -1,26 +1,37 @@
 package kadai;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author ryo
  *
  */
 public class KisoKadai01 {
-	public static void main(String[] args){
+	public static  void main(String[] args) throws IOException{
 		System.out.println("[ 九九表 ]");
 
-
+		int x;
+		int y;
 	while ( true){
-		System.out.println("Xは");
-		int x = new Scanner(System.in).nextInt();
-		System.out.println("Yは");
-		int y = new Scanner(System.in).nextInt();
-		for(int i = 1; i <=x; i++){
-			for(int j = 1; j <=y; j++){
-				System.out.printf(" %2d", i * j);
-				}
-			System.out.println();
+		try{
+			System.out.println("Xは");
+			BufferedReader br =new BufferedReader (new InputStreamReader (System.in));
+	        String tate = br.readLine( );
+	         x= Integer.parseInt(tate);
+	        System.out.println("Yは");
+	        BufferedReader br2 =new BufferedReader (new InputStreamReader (System.in));
+	        String yoko = br2.readLine( );
+	         y = Integer.parseInt(yoko);
+	        	 for(int i = 1; i <=x; i++){
+	        		  for(int j = 1; j <=y; j++){
+	        			  System.out.printf(" %2d", i * j);
+	        		  }
+	        		  System.out.println();
+	        	 }
+		}catch ( NumberFormatException e ) {
+			System.out.println( "数値に変換できる文字を入力してください" );
 		}
 	}
 	}
