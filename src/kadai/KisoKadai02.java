f12d4f51e6521a7fcf52a394d0086d692fb02a90
Masaki <vkiie8564hbgfg@gmail.com>
2016-01-26 12:54:46 +0900

    基礎０２

--- src/kadai/KisoKadai02.java
+++ src/kadai/KisoKadai02.java
@@ -1,12 +1,13 @@
 package kadai;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class KisoKadai02 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int answer;
 		int my;
-		int r;
 		int counter = 0;
 		System.out.println("[数当てゲーム]");
 		System.out.println("1～100の数を当てよう。");
@@ -14,19 +15,25 @@ public class KisoKadai02 {
 		answer = (int)(Math.random() * (100+1));
 			while(true) {
 				counter ++;
-				my =  new Scanner(System.in).nextInt();
+				try{
+					BufferedReader br =new BufferedReader (new InputStreamReader (System.in));
+					String kaku = br.readLine( );
+					my =  Integer.parseInt(kaku);
 
-				if(my < answer) {
-					System.out.println(my + "よりは大きい。");
-				} else if(my > answer){
-					System.out.println(my + "よりは小さい。");
-				} else{
-					System.out.println("正解!");
-					break;
+					if(my < answer) {
+						System.out.println(my + "よりは大きい。");
+					} else if(my > answer){
+						System.out.println(my + "よりは小さい。");
+					} else{
+						System.out.println("正解!");
+						break;
+					}
+				}catch ( NumberFormatException e ) {
+					System.out.println( "数値に変換できる文字を入力してください" );
 				}
+				System.out.println("正解までに " + counter + "回かかりました。");
 			}
-
-			System.out.println("正解までに " + counter + "回かかりました。");
 		}
 	}
+
 
